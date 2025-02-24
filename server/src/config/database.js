44f6f062cfe6472294c8c8cd49a1cd453e8f47a9
Filipe Magalhães Sena <94627887+fmsena1@users.noreply.github.com>
2025-02-24 01:13:45 -0300

    chore: initial commit

--- server/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('@sequelize/core');
-const { SqliteDialect } = require('@sequelize/sqlite3');
-
-const sequelize = new Sequelize({
-  dialect: SqliteDialect,
-  storage: 'sequelize.sqlite',
-});
-
-module.exports = sequelize;
