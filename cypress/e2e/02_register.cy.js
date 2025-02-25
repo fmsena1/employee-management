31954f931e81667daf1b76dc827c83fa8b8c9a65
Filipe Magalhães Sena <94627887+fmsena1@users.noreply.github.com>
2025-02-25 15:23:48 -0300

    chore: refactor login and registration tests to remove commented code and improve readability

--- cypress/e2e/02_register.cy.js
+++ cypress/e2e/02_register.cy.js
@@ -1,28 +1,28 @@
-// describe('Register Page', () => {
-//   beforeEach(() => {
-//     cy.visit('/register');
-//   });
+describe('Register Page', () => {
+  beforeEach(() => {
+    cy.visit('/register');
+  });
 
-//   it('should display the register form', () => {
-//     cy.get('.register-page').should('be.visible');
-//     cy.get('input[type="email"]').should('be.visible');
-//     cy.get('input[type="password"]').should('be.visible');
-//     cy.get('button[type="submit"]').should('be.visible');
-//   });
+  it('should display the register form', () => {
+    cy.get('.register-page').should('be.visible');
+    cy.get('input[type="email"]').should('be.visible');
+    cy.get('input[type="password"]').should('be.visible');
+    cy.get('button[type="submit"]').should('be.visible');
+  });
 
-//   it('should navigate to the login page', () => {
-//     cy.contains('a', 'Login').click();
-//     cy.url().should('include', '/login');
-//   });
+  it('should navigate to the login page', () => {
+    cy.contains('a', 'Login').click();
+    cy.url().should('include', '/login');
+  });
 
-//   it('should register successfully with valid credentials', () => {
-//     const randomEmail = `newuser${Cypress._.random(0, 1e6)}@example.com`;
-//     const randomPassword = `validpassword${Cypress._.random(0, 1e6)}`;
+  it('should register successfully with valid credentials', () => {
+    const randomEmail = `newuser${Cypress._.random(0, 1e6)}@example.com`;
+    const randomPassword = `validpassword${Cypress._.random(0, 1e6)}`;
 
-//     cy.get('input[type="email"]').type(randomEmail);
-//     cy.get('input[type="password"]').type(randomPassword);
-//     cy.get('button[type="submit"]').click();
-//     cy.url().should('not.include', '/register');
-//     cy.get('.q-notification').should('contain', 'Cadastro realizado com sucesso!');
-//   });
-// });
+    cy.get('input[type="email"]').type(randomEmail);
+    cy.get('input[type="password"]').type(randomPassword);
+    cy.get('button[type="submit"]').click();
+    cy.url().should('not.include', '/register');
+    cy.get('.q-notification').should('contain', 'Cadastro realizado com sucesso!');
+  });
+});
